d3b7d1a523a4d58fd599f7d1ce7f157cb6711dd6
Toufiq A. Shishir <toufiq_a24@outlook.com>
2024-04-20 15:39:46 +0600

    modified fish theme and character

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,15 @@
-fish_add_path -g /home/toufiq/.local/bin /usr/bin /usr/sbin /home/linuxbrew/.linuxbrew/bin /usr/local/go/bin
+fish_add_path -g /home/toufiq/.local/bin /usr/bin /usr/sbin /home/linuxbrew/.linuxbrew/bin /usr/local/go/bin /usr/lib
 
 zoxide init fish | source
+
+fish_config theme choose "Rosé Pine Moon"
+
+set -g tide_character_color f6c177
+set -g tide_character_color_failure f6c177
+set -g tide_character_icon '>>>'
+set -g tide_character_vi_icon_default '>>>'
+set -g tide_character_vi_icon_replace '>>>'
+set -g tide_character_vi_icon_visual '>>>'
 
 if status is-interactive
     for file in ~/progs/dotfiles/fish/abbreviations/*.fish
