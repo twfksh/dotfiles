921d674a0eabb536560ec2cc704aeda32cfeb94b
Toufiq A. Shishir <toufiq_a24@outlook.com>
2024-07-11 21:56:52 +0600

    -

---
+++ nvim/.config/nvim/lua/colors/init.lua
@@ -0,0 +1,11 @@
+return {
+  "tjdevries/colorbuddy.nvim",
+  dependencies = {
+    { "echasnovski/mini.base16", version = "*" },
+    { "EdenEast/nightfox.nvim" },
+    { "sainnhe/everforest" },
+  },
+  config = function ()
+    vim.cmd[[colorscheme terafox]]
+  end
+}
